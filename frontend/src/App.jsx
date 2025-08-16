4cd2f924ac0340a706be5d2e22c615c128c2be4a
UjjwalMishra01 <ujjwalmishra523@gmail.com>
2025-08-17 00:22:56 +0530

    Fixed vercel analytics import and added background image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 import axios from 'axios'; 
 import {
   Box,
@@ -13,6 +14,7 @@ import {
   Button,
   CircularProgress, 
 } from '@mui/material';
+
 
 
 function App() {
@@ -106,7 +108,9 @@ function App() {
           </Box>
         )
       }
+       <Analytics />
     </Container>
+    
   );
 }
 
