f567292d48753c0d0fb1608863dbc01da8349cb9
UjjwalMishra01 <ujjwalmishra523@gmail.com>
2025-08-29 18:56:26 +0530

    Update the api endpoint to render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
      setLoading(true)
      setError('');
      try{
-     const response = await axios.post("http://localhost:8080/api/email/generate", {
+     const response = await axios.post("https://replywise-backend-1.onrender.com/api/email/generate", {
       emailContent,
       tone
      });
